68e259a58ab3b34310c71c0ff5a0eb469af2d780
AAlkamel <68661204+AAlkamel@users.noreply.github.com>
2024-01-24 14:38:13 +0300

    02

--- Problem #2/Algorithm1-02.cpp
+++ Problem #2/Algorithm1-02.cpp
@@ -6,8 +6,14 @@ using namespace std;
 void PrintName(string name) {
 	cout << "\n\nyour name is: " + name +"\n\n"<<endl;
 }
-
+string ReadName(){
+	string name;
+	cout << "\n\nenter your name pleace.\n";
+	// cin >> name;
+	getline(cin,name);
+	return name;
+}
 int main()
 {
-	PrintName("my_name");
+	PrintName(ReadName());
 }
